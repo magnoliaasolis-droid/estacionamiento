5c15b19d09e63d777399881b9d6b12e42bba6234
Magno <magnoliaasolis@gmail.com>
2026-04-23 01:36:16 -0600

    fix 502

--- app.py
+++ app.py
@@ -5,12 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def inicio():
+    return "Servidor activo"
+
+@app.route("/testdb")
+def testdb():
     try:
         conexion = mysql.connector.connect(
             host="shortline.proxy.rlwy.net",
             user="root",
+            port="47707",
             password="fHLnKJtzHfArjeDLubPQmnntlJrTOTYt",
             database="railway"
+            connection_timeout=5
         )
 
         return "Conectado a MySQL desde Render"
