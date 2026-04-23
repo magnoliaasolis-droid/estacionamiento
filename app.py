a55f193626128376168393994d37597a76b2a4c7
Magno <magnoliaasolis@gmail.com>
2026-04-23 01:56:00 -0600

    conexion railway y ver datos

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, jsonify
 import mysql.connector
 
 app = Flask(__name__)
+
+# Datos Railway
+DB_CONFIG = {
+    "host": "shortline.proxy.rlwy.net",
+    "port": 47707,
+    "user": "root",
+    "password": "fHLnKJtzHfArjeDLubPQmnntlJrTOTYt",
+    "database": "railway"
+}
 
 @app.route("/")
 def inicio():
@@ -10,14 +19,25 @@ def inicio():
 @app.route("/testdb")
 def testdb():
     try:
-        conexion = mysql.connector.connect(
-            host="shortline.proxy.rlwy.net",
-            port=47707,
-            user="root",
-            password="fHLnKJtzHfArjeDLubPQmnntlJrTOTYt",
-            database="railway",
-            connection_timeout=5
-        )
+        conexion = mysql.connector.connect(**DB_CONFIG)
+        conexion.close()
         return "Conectado a Railway"
+    except Exception as e:
+        return str(e)
+
+@app.route("/ver")
+def ver():
+    try:
+        conexion = mysql.connector.connect(**DB_CONFIG)
+        cursor = conexion.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM registro")
+        datos = cursor.fetchall()
+
+        cursor.close()
+        conexion.close()
+
+        return jsonify(datos)
+
     except Exception as e:
         return str(e)
